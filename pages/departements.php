9187f5b10694f4f0efe6f85eeddd990d7fe623f8
juliobasito <bassecathalinat.jules@gmail.com>
2017-04-20 11:53:18 +0200

    departement

--- pages/departements.php
+++ pages/departements.php
@@ -13,13 +13,31 @@
 		<li><button type="button" class="btn btn-primary"><a href="#">Departement</a></button></li>
 		<li><button type="button" class="btn btn-primary"><a href="regions.php">Region</a></button></li>
 	</ul>
+	
 	<?php
-		$can = new DomDocument();
-		$can->load('http://www.interieur.gouv.fr/avotreservice/elections/telechargements/EssaiPR2017/referencePR/listeregdptcom.xml');
-		$listeCandidat = $can->getElementsByTagName('LibDpt');
-		foreach($listeCandidat as $liste){
-			 echo $liste->firstChild->nodeValue.'</br>';
+		if(!empty($_POST["valider"])){
+			$explode = explode(".", $_POST["departement"]);
+			$region = $explode[0];
+			$departeemnt = $explode[1];
+			$xml = simplexml_load_file('http://www.interieur.gouv.fr/avotreservice/elections/telechargements/EssaiPR2017/resultatsT1/'.$region.'/'.$departement.'/'.$departement.'.xml');
+			foreach($xml->Region->Tours->Tour->Resultats->Candidats->Candidat as $candidat){
+				echo $candidat->NomPsn." a obtenu ".$candidat->NbVoix." voix, bien joue !</br>";
+			}
 		}
+		$xml = simplexml_load_file('http://www.interieur.gouv.fr/avotreservice/elections/telechargements/EssaiPR2017/referencePR/listeregdptcom.xml');
+
 	?>
+	<form method="post">
+		<select name="region">
+			<?php
+				foreach($xml->Regions->Region as $regions){
+					foreach($regions->Departements->Departement as $departement){
+						echo '<option value="'.$regions->CodReg3Car.'.'.$departement->CodDpt3Car.'>'.$departement->LibDpt.'</option>';
+					}
+				}
+			?>
+		</select>uhkjhj
+		<input type="submit" value="valider" name="valider"/>
+	</form>
 </body>
 </html>
