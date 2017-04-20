a1787ef632f61dd2e98dadbd3f8c7b1920da1926
juliobasito <bassecathalinat.jules@gmail.com>
2017-04-20 10:36:30 +0200

    ajout des departements/region/communes

--- pages/departements.php
+++ pages/departements.php
@@ -6,17 +6,17 @@
 	<link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-	<h1>Regions</h1>
+	<h1>Departement</h1>
 	<ul class="list-inline">
 		<li><button type="button" class="btn btn-primary"><a href="../">France</a></button></li>
 		<li><button type="button" class="btn btn-primary"><a href="communes.php">Communes</a></button></li>
-		<li><button type="button" class="btn btn-primary"><a href="departements.php">Departement</a></button></li>
-		<li><button type="button" class="btn btn-primary"><a href="#">Region</a></button></li>
+		<li><button type="button" class="btn btn-primary"><a href="#">Departement</a></button></li>
+		<li><button type="button" class="btn btn-primary"><a href="regions.php">Region</a></button></li>
 	</ul>
 	<?php
 		$can = new DomDocument();
 		$can->load('http://www.interieur.gouv.fr/avotreservice/elections/telechargements/EssaiPR2017/referencePR/listeregdptcom.xml');
-		$listeCandidat = $can->getElementsByTagName('LibReg');
+		$listeCandidat = $can->getElementsByTagName('LibDpt');
 		foreach($listeCandidat as $liste){
 			 echo $liste->firstChild->nodeValue.'</br>';
 		}
