97619855ed53ade1e95b45dc54b25dc2cd0e651f
juliobasito <bassecathalinat.jules@gmail.com>
2017-04-20 11:28:58 +0200

    add region

--- pages/regions.php
+++ pages/regions.php
@@ -14,12 +14,24 @@
 		<li><button type="button" class="btn btn-primary"><a href="#">Region</a></button></li>
 	</ul>
 	<?php
-		$can = new DomDocument();
-		$can->load('http://www.interieur.gouv.fr/avotreservice/elections/telechargements/EssaiPR2017/referencePR/listeregdptcom.xml');
-		$listeCandidat = $can->getElementsByTagName('LibReg');
-		foreach($listeCandidat as $liste){
-			 echo $liste->firstChild->nodeValue.'</br>';
+		if(!empty($_POST["valider"])){
+			$xml = simplexml_load_file('http://www.interieur.gouv.fr/avotreservice/elections/telechargements/EssaiPR2017/resultatsT1/'.$_POST['region'].'/'.$_POST["region"].'.xml');
+			foreach($xml->Region->Tours->Tour->Resultats->Candidats->Candidat as $candidat){
+				echo $candidat->NomPsn." a obtenu ".$candidat->NbVoix." voix, bien joue !</br>";
+			}
 		}
+		$xml = simplexml_load_file('http://www.interieur.gouv.fr/avotreservice/elections/telechargements/EssaiPR2017/referencePR/listeregdptcom.xml');
+
 	?>
+	<form method="post">
+		<select name="region">
+			<?php
+				foreach($xml->Regions->Region as $regions){
+					echo '<option value="'.$regions->CodReg3Car.'">'.$regions->LibReg.'</option>';
+				}
+			?>
+		</select>
+		<input type="submit" value="valider" name="valider"/>
+	</form>
 </body>
 </html>
