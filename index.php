230f26b9b79841571584bf8b27893ec7cc6d4719
loukahanuise <louka.hanuise@ynov.com>
2017-04-20 14:31:37 +0200

    header on index

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<ul class="list-inline">
-		<li><button type="button" class="btn btn-primary">France</button></li>
-		<li><button type="button" class="btn btn-primary">Communes</button></li>
-		<li><button type="button" class="btn btn-primary">Departement</button></li>
-		<li><button type="button" class="btn btn-primary"><a href="pages/regions.php">Region</a></button></li>
-	</ul>
+	<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+		<li><a href="../pages/">France</a></li>
+		<li><a href="pages/communes.php">Communes</a></li>
+		<li><a href="pages/departements.php">Departement</a></li>
+		<li><a href="pages/regions.php">Region</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+
+</nav>
 	<?php
 		$can = new DomDocument();
 		$can->load('http://www.interieur.gouv.fr/avotreservice/elections/telechargements/EssaiPR2017/candidatureT1/CandidatureT1.xml');
