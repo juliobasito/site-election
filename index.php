cb3930a6daa62dc10ca3ca79732724ed1229d0de
nexon <yohann.bernard@ynov.com>
2017-04-20 14:44:43 +0200

    Map + chart bar

--- index.php
+++ index.php
@@ -1,42 +1,155 @@
+<?php
+$fe = simplexml_load_file('http://www.interieur.gouv.fr/avotreservice/elections/telechargements/EssaiPR2017/resultatsT1/FE.xml');
+
+$candidats = array();
+foreach ($fe->Tours->Tour->Resultats->Candidats->Candidat as $candidat) {
+    $candidats[] = (array)$candidat;
+}
+
+$price = array();
+foreach ($candidats as $key => $row) {
+    $price[$key] = $row['RapportExprime'];
+}
+array_multisort($price, SORT_DESC, $candidats);
+?>
+
+
 <html>
 <head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="../css/bootstrap.min.css">
-	<link rel="stylesheet" href="../css/bootstrap-theme.min.css">
-	<link rel="stylesheet" href="../css/style.css">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css"src="https://cdnjs.cloudflare.com/ajax/libs/jvectormap/2.0.4/jquery-jvectormap.css">
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+    <script src="jvectormap/jquery.jvectormap.min.js"></script>
+    <script src="http://jvectormap.com/js/jquery-jvectormap-fr_regions-mill.js"></script>
+    <script src="http://jvectormap.com/js/jquery-jvectormap-fr-mill.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
 </head>
 <body>
-	<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../pages/">France</a></li>
+                <li><a href="pages/communes.php">Communes</a></li>
+                <li><a href="pages/departements.php">Departement</a></li>
+                <li><a href="pages/regions.php">Region</a></li>
+            </ul>
+        </div>
     </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-		<li><a href="../pages/">France</a></li>
-		<li><a href="pages/communes.php">Communes</a></li>
-		<li><a href="pages/departements.php">Departement</a></li>
-		<li><a href="pages/regions.php">Region</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-
 </nav>
-	<?php
-		$can = new DomDocument();
-		$can->load('http://www.interieur.gouv.fr/avotreservice/elections/telechargements/EssaiPR2017/candidatureT1/CandidatureT1.xml');
-		$listeCandidat = $can->getElementsByTagName('NomPsn');
-		foreach($listeCandidat as $liste){
-			 echo $liste->firstChild->nodeValue;
-		}
-	?>
+<div class="container">
+
+    <button id="search">Actualiser</button>
+    <div id="map-container">
+        <div id="map" style="width: 1000px; height: 1000px;"></div>
+    </div>
+
+    <canvas id="myChart"></canvas>
+
+</div>
+
+
 </body>
+<script>
+    ////////CHART////////
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [
+                <?php
+                foreach ($candidats as $candidat) {
+                    echo '"' . $candidat['NomPsn'] . ' ' . $candidat['PrenomPsn'] . '",';
+                }
+                echo '""';
+                ?>
+            ],
+            datasets: [{
+                backgroundColor: [
+                    '#E50077',
+                    '#E10001',
+                    '#DE7100',
+                    '#D4DA00',
+                    '#61D700',
+                    '#00D30D',
+                    '#00D078',
+                    '#00B8CC',
+                    '#004DC9',
+                    '#1A00C5',
+                    '#7E00C2',
+                    '#BF009E',
+                    'transparent'
+                ],
+                data: [
+                    <?php
+                    foreach ($candidats as $candidat) {
+                        echo str_replace(',', '.', $candidat['RapportExprime']) . ',';
+                    }
+                    echo 50;
+                    ?>
+                ]
+
+            }]
+        }
+    });
+    ////////CHART////////
+
+
+    ////////AJAX////////
+    var host = 'http://www.interieur.gouv.fr/avotreservice/elections/telechargements/EssaiPR2017/resultatsT1/';
+
+
+    function search() {
+
+        $.ajax({
+            type: 'GET',
+            url: host+'FE.xml',
+            data: '',
+            crossDomain: true,
+            success: function (data, xhr) {
+                console.log('revceived');
+                console.log(data);
+            },
+            error: function (data, xhr) {
+                console.log('error');
+                console.log(data);
+            }
+        });
+
+        $('#map-container').html(' <div id="map"></div>');
+        $('#map').vectorMap({
+            map: 'fr_mill'
+        });
+
+    }
+
+    $('#map').click(function () {
+        search();
+        console.log('yes');
+    });
+    ////////CHART////////
+
+
+    $(function () {
+        $('#map').vectorMap({
+            map: 'fr_regions_mill'
+        });
+    });
+</script>
 </html>
